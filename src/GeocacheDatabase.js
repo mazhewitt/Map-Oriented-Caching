5daeb0a86753359ff3b5e209576f0c80126f2c27
Mazda Hewitt <mazda@aduki.co.uk>
2012-08-17 23:15:28 +0200

    GeocacheDatabase update when geocache is already in the database

--- src/GeocacheDatabase.js
+++ src/GeocacheDatabase.js
@@ -4,10 +4,10 @@ var GeocacheDatabase = (function(){
 
     var database = null;
     var _initialised = false;
-	
-
-	var	GEOCACHE_NOT_FOUND = "GEOCACHE_NOT_FOUND";
-	var	DATABASE_ERROR = "DATABASE_ERROR";
+    
+    
+    var GEOCACHE_NOT_FOUND = "GEOCACHE_NOT_FOUND";
+    var DATABASE_ERROR = "DATABASE_ERROR";
     
     var init = function(){
         return initialiseDatabase().pipe(createTable());
@@ -40,9 +40,35 @@ var GeocacheDatabase = (function(){
     };
     
     var store = function(geocache){
+        var storingGeocache = new $.Deferred();
+        
+        var loadPromise = loadByGCCode(geocache.GCCode).done(function(gc){
+            saveToDatabase(geocache, true).done(function(){
+                storingGeocache.resolve();
+            });
+        }).fail(function(code, err){
+            if (code === GEOCACHE_NOT_FOUND) {
+                saveToDatabase(geocache, false).done(function(){
+                    storingGeocache.resolve();
+                });
+            }
+            else {
+                storingGeocache.reject(code, err);
+            }
+        });
+        
+        return storingGeocache.promise();
+    };
+    
+    var saveToDatabase = function(geocache, shouldUpdate){
         var savingGeocache = new $.Deferred();
         database.transaction(function(tx){
-            tx.executeSql('INSERT INTO geocache(GUID, GCCode, lat, lon, GPXFile) VALUES (?,?,?,?,?)', [geocache.GUID, geocache.GCCode, geocache.mainCoordinate.lat, geocache.mainCoordinate.lon, geocache.gpxFile]);
+            if (shouldUpdate) {
+                tx.executeSql('UPDATE geocache set GUID=?, lat=?, lon=?, GPXFile=? where GCCode = ?', [geocache.GUID, geocache.mainCoordinate.lat, geocache.mainCoordinate.lon, geocache.gpxFile, geocache.GCCode]);
+            }
+            else {
+                tx.executeSql('INSERT INTO geocache(GUID, GCCode, lat, lon, GPXFile) VALUES (?,?,?,?,?)', [geocache.GUID, geocache.GCCode, geocache.mainCoordinate.lat, geocache.mainCoordinate.lon, geocache.gpxFile]);
+            }
         }, function(err){
             savingGeocache.reject(DATABASE_ERROR, err.message);
         }, function(tx, results){
@@ -51,46 +77,46 @@ var GeocacheDatabase = (function(){
         return savingGeocache.promise();
     };
     
-	var loadByGCCode= function(gcCode){
-		var loadingGeocache = new $.Deferred();
-		database.transaction(function(tx){
-			tx.executeSql('SELECT GPXFile from geocache where GCCode = ?', [gcCode], function(tx, results){
-				if (results.rows.length < 1) {
-					savingGeocache.reject(GEOCACHE_NOT_FOUND, gcCode + " not found in database");
-				}
-				else {
-					var gc = new Geocache();
-					gc.init(results.rows.item(0).GPXFile);
-					loadingGeocache.resolve(gc);
-				}
-			}), function(err){
-				loadingGeocache.reject(DATABASE_ERROR, err.message);
-			}
-		});
+    var loadByGCCode = function(gcCode){
+        var loadingGeocache = new $.Deferred();
+        database.transaction(function(tx){
+            tx.executeSql('SELECT GPXFile from geocache where GCCode = ?', [gcCode], function(tx, results){
+                if (results.rows.length < 1) {
+                    loadingGeocache.reject(GEOCACHE_NOT_FOUND, gcCode + " not found in database");
+                }
+                else {
+                    var gc = new Geocache();
+                    gc.init(results.rows.item(0).GPXFile);
+                    loadingGeocache.resolve(gc);
+                }
+            }), function(err){
+                loadingGeocache.reject(DATABASE_ERROR, err.message);
+            }
+        });
         return loadingGeocache.promise();
-	};
-	
+    };
+    
     
     var public_interface = {
         init: init,
-        store: store, 
-		loadByGCCode: loadByGCCode, 
-		GEOCACHE_NOT_FOUND: GEOCACHE_NOT_FOUND, 
-		DATABASE_ERROR: DATABASE_ERROR
+        store: store,
+        loadByGCCode: loadByGCCode,
+        GEOCACHE_NOT_FOUND: GEOCACHE_NOT_FOUND,
+        DATABASE_ERROR: DATABASE_ERROR
     };
-	
+    
     Object.defineProperty(public_interface, "initialised", {
         get: function(){
             return _initialised;
         }
     });
-	
+    
     Object.defineProperty(public_interface, "database", {
         get: function(){
             return database;
         }
     });
-	
+    
     return public_interface;
     
 })();
