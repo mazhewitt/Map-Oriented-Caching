e14da2ddfb3f416837e4e11c2e837193ffc4cc40
libra guest <mazda@aduki.co.uk>
2012-10-21 05:12:57 -0400

    small clean up

--- src/GeocacheMapUILayer.js
+++ src/GeocacheMapUILayer.js
@@ -14,63 +14,62 @@ var GeocacheMapUILayer = L.Class.extend({
         opacity: 1
     },
 
-    _waypointMarkers : [],
-    
+    _waypointMarkers: [],
+
     getGeocache: function() {
         return this._geocache;
     },
-        
-    setGeocache: function(cache){
-    	this._geocache = cache;
+
+    setGeocache: function(cache) {
+        this._geocache = cache;
         this._initWaypoints();
-    },        
-
-    initialize: function(gc) { 
-       this.setGeocache(gc);
     },
 
-    _initWaypoints: function(){
-    	var waypoints = this._geocache.waypoints;
-    	this._waypointMarkers = [];
-    	
-    	for (var i =0; i < waypoints.length; i++){
-    		var waypointMarkerOptions = {
-    			icon: new L.Icon.Default(),
-    			title: waypoints[i].name,
-    			clickable: true,
-    			draggable: false,
-    			zIndexOffset: 0,
-    			opacity: 1
-    		};
-    		
-    		var latLng = new L.LatLng(waypoints[i].coordinate.lat, waypoints[i].coordinate.lon);
-    		
-    		this._waypointMarkers[i] = new L.Marker(latLng, waypointMarkerOptions);
-    	}
+    initialize: function(gc) {
+        this.setGeocache(gc);
     },
-    
-    
-    addTo: function(map) {
-        map.addLayer(this);
-        return this;
-    },  
 
-    onAdd: function (map) {
-        this._map = map;
+    _initWaypoints: function() {
         var waypoints = this._geocache.waypoints;
-        for (var i =0; i < waypoints.length; i++){
-        	this._waypointMarkers[i].addTo(map);
+        this._waypointMarkers = [];
+
+        for (var i = 0; i < waypoints.length; i++) {
+            var waypointMarkerOptions = {
+                icon: new L.Icon.Default(),
+                title: waypoints[i].name,
+                clickable: true,
+                draggable: false,
+                zIndexOffset: 0,
+                opacity: 1
+            };
+
+            var latLng = new L.LatLng(waypoints[i].coordinate.lat, waypoints[i].coordinate.lon);
+
+            this._waypointMarkers[i] = new L.Marker(latLng, waypointMarkerOptions);
         }
     },
 
-    onRemove: function (map) {
+
+    addTo: function(map) {
+        map.addLayer(this);
+        return this;
+    },
+
+    onAdd: function(map) {
+        this._map = map;
+        var waypoints = this._geocache.waypoints;
+        for (var i = 0; i < waypoints.length; i++) {
+            this._waypointMarkers[i].addTo(map);
+        }
+    },
+
+    onRemove: function(map) {
         // remove layer's DOM elements and listeners
-    	 var waypoints = this._geocache.waypoints;
-         for (var i =0; i < waypoints.length; i++){
-         	map.removeFrom(map);
-         }
+        var waypoints = this._geocache.waypoints;
+        for (var i = 0; i < waypoints.length; i++) {
+            map.removeFrom(map);
+        }
     }
 
-  
-});
 
+});
